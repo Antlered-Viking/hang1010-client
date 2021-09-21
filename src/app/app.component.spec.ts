2c8980e413c114e86d3b827a7cbfb709bd1eb70c
NeonSpork <leiknes@gmail.com>
2021-09-21 22:02:10 +0200

    refactor: update tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hang1010-client'`, () => {
+  it(`should have as title 'Hang1010'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hang1010-client');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hang1010-client app is running!');
+    expect(app.title).toEqual('Hang1010');
   });
 });
